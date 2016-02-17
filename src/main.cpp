99efccd04d258462d1eefebfea2bc75653292524
gilucioli <gilucioli@gmail.com>
2016-02-17 13:07:27 -0200

    alteraçao na funcao calcInitialSafetyLevelInst
    Agora ela ta virando a funcao construtivo.
    Teremos que renomear ela, ou separar as funcoes ainda.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 
 using namespace std;
 
+
+
+
 int main()
 {
     XMLDataLoader loader;
@@ -13,8 +16,13 @@ int main()
     loader.loadInputData("Instance_V_1.1.xml","./input");
 
     Solution* solution = new Solution();
-    printf("\n\n%s",solution->toString(true).c_str());
-    solution->calcCost(true);
-    printf("\n\nCost = %f\n",solution->getCost());
+    printf("\n");
+    solution->calcInitialSafetyLevelInst(InputData::getCustomers(), 120);
+    printf("dividir customers por caminhao\n");
+//    printf("\n\n%s",solution->toString(true).c_str());
+//    solution->calcCost(true);
+//    printf("\n\nCost = %f\n",solution->getCost());
 
 }
+
+
